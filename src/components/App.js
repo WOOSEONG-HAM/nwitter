2a060633c365173621f60c50318c233676b65871
WOOSEONG-HAM <wooseongham@gmail.com>
2020-12-14 12:26:58 +0900

    초기 router 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AppRouter from "./Router";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <AppRouter />
+    </>
   );
 }
 
