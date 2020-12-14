ae3d72d46d55c6ebd9daa1102be5fa5fb3a401ca
WOOSEONG-HAM <wooseongham@gmail.com>
2020-12-14 16:22:35 +0900

    로그아웃 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react';
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
-import Home from 'routes/Home';
-import Auth from 'routes/Auth';
+import React, { useState } from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
-const AppRouter = ({isLoggedIn}) => {
-    return (
+const AppRouter = ({ isLoggedIn }) => {
+  return (
     <Router>
-        <Switch>
-            {isLoggedIn ? 
-            <>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
             <Route exact path="/">
-                <Home />
+              <Home />
             </Route>
-            </> : (
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
+        ) : (
+          <>
             <Route exact path="/">
-                <Auth />
+              <Auth />
             </Route>
-             ) }
-
-        </Switch>
+          </>
+        )}
+      </Switch>
     </Router>
-    )
-}
-
+  );
+};
 export default AppRouter;
