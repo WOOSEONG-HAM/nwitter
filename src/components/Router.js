afb6dfc0b9e909d8f8b6effae3e495a6f048309c
WOOSEONG-HAM <wooseongham@gmail.com>
2020-12-16 22:39:53 +0900

    트위터 디자인 적용

--- src/components/Router.js
+++ src/components/Router.js
@@ -11,14 +11,14 @@ const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
       {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
-          <>
+          <div className="contentBox">
             <Route exact path="/">
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
               <Profile userObj={userObj} refreshUser={refreshUser} />
             </Route>
-          </>
+          </div>
         ) : (
           <>
             <Route exact path="/">
